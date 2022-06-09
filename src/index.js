d0039b7343196fd3b4e11d8ae1ec62225385e84e
unknown <bgw595@myy.haaga-helia.fi>
2022-06-09 19:50:13 +0300

    indexjs updated to react17 v

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -17,9 +17,9 @@ function Main() {
   )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Main />
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
